79607e8b8c8957df740478f3c2b7788115ede187
Md. Shajalal Sojib <shahjalalsojib088@gmail.com>
2024-10-08 19:42:31 +0600

    Create operators.c
    C Operators

---
+++ operators.c
@@ -0,0 +1,12 @@
+  #include<stdio.h>
+  int main(){
+     int numder1 = 20;
+     int numder2 = numder1+ 50;
+     int numder3 = numder2+numder1;
+
+     printf("Enter the numder1 : %d\n",numder1);
+     printf("Enter the numder2 : %d\n",numder2);
+     printf("Enter the numder3 : %d\n",numder3);
+
+     return 0;
+  }
