8d0c64a2b750873a4842a47884c3c3dfc474392c
Md. Shajalal Sojib <shahjalalsojib088@gmail.com>
2024-10-08 18:46:45 +0600

    Create C_Variables.c
    Format Specifiers

---
+++ C_Variables.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+     int numder1 = 20;
+     float numder2 = 59.89;
+     char Name = 'S';
+     printf("Enter a int numder %d\n",numder1);
+     printf("Enter a float numder%f\n ",numder2);
+     printf("Enter a char name %c\n",Name);
+
+
+
+}
