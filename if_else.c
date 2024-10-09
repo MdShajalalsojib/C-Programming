80b44ab303e81b7c758c13c962ff94e15b94425f
Md. Shajalal Sojib <shahjalalsojib088@gmail.com>
2024-10-09 09:25:48 +0600

    Create if_else.c
    C_if_else

---
+++ if_else.c
@@ -0,0 +1,14 @@
+ #include<stdio.h>
+ int main(){
+   int number;
+   printf("Enter a any number : ");
+   scanf("%d",number);
+   
+   if (number%2==0){
+      printf("%d is an even number : ",number);
+   }
+   else{
+    printf("%d is an odd number : ",number);
+   }
+   return 0;
+ }
