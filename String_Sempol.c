48ec85bf9afb38efe8960710f163c1db7ba464df
Md. Shajalal Sojib <shahjalalsojib088@gmail.com>
2024-10-20 23:16:22 +0600

    Create String_Sempol.c
    String sempol.c

---
+++ String_Sempol.c
@@ -0,0 +1,18 @@
+/* #include<stdio.h>
+ int main()
+ {
+     char s1[ ] = "Shajalal Sojib";
+     printf("s1 = %s\n",s1);
+ }
+ */
+
+
+
+ #include<stdio.h>
+ int main()
+ {
+     char s1[100] ;
+     printf ("Enter your full name:");
+     gets (s1);
+     printf ("Full name = %s\n",s1);
+ }
