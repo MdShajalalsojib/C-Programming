25812c84f52fb4811b6763e216dcdc21b967840e
Md. Shajalal Sojib <shahjalalsojib088@gmail.com>
2024-11-13 21:53:11 +0600

    Update Nested_Loops.c

--- Nested_Loops.c
+++ Nested_Loops.c
@@ -1,29 +1,22 @@
-#include<stdio.h>
-int main(){
-   int row = 5;
-   int column = 4;
-   for(int i= 1;i<= row; i++){
-    for(int j = 1; j<=column;j++){
-        printf("*  ");
-    }
-    printf("\n");
-   }
-   return 0;
+// C program to print pattern using nested while loops 
+#include <stdio.h> 
+  
+int main() 
+{ 
+    int end = 5; 
+  
+    printf("Pattern Printing using Nested While loop"); 
+  
+    int i = 1; 
+  
+    while (i <= end) { 
+        printf("\n"); 
+        int j = 1; 
+        while (j <= i) { 
+            printf("%d ", j); 
+            j = j + 1; 
+        } 
+        i = i + 1; 
+    } 
+    return 0; 
 }
-/*
-#include<stdio.h>
-int main(){
-   int weeks=3, days_in_week = 7;
-   for(int i = 1;i<=weeks;i++){
-    printf("Week: \n",i);
-    for(int j = 1;j <= days_in_week;j++){
-        //continue if the day is an odd number
-        if((j % 2 != 0)){
-            continue;
-        }
-        prontf("    Day: \n",j);
-    }
-   }
-   
-}
-*\
