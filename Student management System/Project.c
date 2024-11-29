6bf3c2e30bb28dce7373d56b272e728c0d7d6fc8
Md. Shajalal Sojib <shahjalalsojib088@gmail.com>
2024-11-29 21:55:41 +0600

    fised bug

--- Student management System/Project.c
+++ Student management System/Project.c
@@ -1,10 +1,15 @@
-#include<stdio.h>
+  #include<stdio.h>
+#include<stdlib.h>
 #include<conio.h>
-void addstudent();
-void studentrecord();
-void searchstudent();
-void delete();
-struct student{
+
+// Function prototypes
+void addStudent();
+void studentRecord();
+void searchStudent();
+void deleteRecord();
+
+// Structure definition
+struct student {
     char first_name[30];
     char last_name[30];
     int roll_no;
@@ -12,94 +17,93 @@ struct student{
     char vill[20];
     float per;
 };
-void main(){
-    int choice;
-    while (choice!=5){
-        printf("\t\t\t======STUDENT DATABASE MANAGEMENT SYSTEM=====");
-        printf("\n\n\n\t\t\t\t  1. Add Student Record\n");
-        printf("\n\n\n\t\t\t\t  2. Student Records\n");
-        printf("\n\n\n\t\t\t\t  3. Search Student\n");
-        printf("\n\n\n\t\t\t\t  4. Delete Record\n");
-        printf("\n\n\n\t\t\t\t  5. Enit\n");
-        printf("\t\t\t\t*****************\n");
-        printf("\t\t\t\t  ");
-        scanf("%d",&choice);
-        switch(choice){
+
+void main() {
+    int choice = 0;
+    while (choice != 5) {
+        system("cls"); // Clear screen
+        printf("\t\t\t====== STUDENT DATABASE MANAGEMENT SYSTEM =====\n");
+        printf("\n\t\t\t\t1. Add Student Record\n");
+        printf("\t\t\t\t2. Student Records\n");
+        printf("\t\t\t\t3. Search Student\n");
+        printf("\t\t\t\t4. Delete Record\n");
+        printf("\t\t\t\t5. Exit\n");
+        printf("\t\t\t\t=====================\n");
+        printf("\t\t\t\tEnter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
             case 1:
-               clrscr();
-               addstudent();
-               clrscr();
-               break;
+                system("cls");
+                addStudent();
+                break;
             case 2:
-               clrscr();
-               studentrecord();
-               printf("\t\t\t\tPress any key to exit\n");
-               getch();
-               clrscr();
-               break;
+                system("cls");
+                //studentRecord();
+                printf("\n\t\t\tPress any key to continue...");
+                getch();
+                break;
             case 3:
-               clrscr();
-               searchstudent();
-               printf("\n\t\t\t\tPress any key to exit\n");
-               getch();
-               clrscr();
-               break;
+                system("cls");
+                //searchStudent();
+                printf("\n\t\t\tPress any key to continue...");
+                getch();
+                break;
             case 4:
-               clrscr();
-               delete();
-               printf("\n\t\t\t\tPress any key to exit\n");
-               getch();
-               clrscr();
-               break;
+                system("cls");
+                //deleteRecord();
+                printf("\n\t\t\tPress any key to continue...");
+                getch();
+                break;
             case 5:
-               clrscr();
-               printf("\n\t\t\t\tThank you, for used this software.\n\n");
-               exit(0);
+                system("cls");
+                printf("\n\t\t\tThank you for using this software.\n\n");
+                exit(0);
                 break;
             default:
-                clrscr();
+                printf("\n\t\t\tInvalid choice! Please try again.\n");
+                printf("\t\t\tPress any key to continue...");
                 getch();
-                printf("\n\t\t\t\tEnter a valid naumber\n\n");
-                printf("\n\t\t\t\tPress any key to continue.......\n");
-                getch();
-                clrscr();
-                break;    
-
+                break;
         }
     }
 }
-void addstudent(){
+
+void addStudent() {
     char another;
     FILE *fp;
     struct student info;
-        do{
-            clrscr();
-            printf("\t\t\t\t======Add Student info=====\n\n\n");
-            fp=fopen("student_info.txt","a");
-            printf("\n\t\t\t\tEnter first name : ");
-            scanf("%s",&info.first_name);
-            printf("\n\t\t\t\tEnter last name : ");
-            scanf("%s",&info.last_name);
-            printf("\n\t\t\tEnter roll no : ");
-            scanf("%d",&info.roll_no);
-            printf("\n\t\t\tEnter Class : ");
-            scanf("%d",&info.Class);
-            printf("\n\t\t\tEnter address : ");
-            scanf("%d",&info.vill);
-            printf("\n\t\t\tEnter percentage : ");
-            scanf("%d",&info.per);
-            printf("\n\t\t\t----------------------------\n");
-            if(fp==NULL){
-                fprintf(stderr,"\t\t\tCan't open file\n");
 
-            }else{
-                printf("\t\t\tRecord stored successfuly\n");
-            }
-            fwrite(&info,sizeof(struct student),1,fp);
-            fclose(fp);
-            printf("\t\t\tDo you want to add another record?(y/n) : ");
-            scanf("%s",&another);
+    do {
+        system("cls");
+        printf("\t\t\t====== Add Student Info =====\n\n");
+        fp = fopen("student_info.dat", "ab");
+        if (fp == NULL) {
+            fprintf(stderr, "Error opening file.\n");
+            return;
+        }
 
-        }while(another=='y'||another=='Y');
-        
+        printf("\n\tEnter First Name: ");
+        scanf("%29s", info.first_name);
+        printf("\n\tEnter Last Name: ");
+        scanf("%29s", info.last_name);
+        printf("\n\tEnter Roll No: ");
+        scanf("%d", &info.roll_no);
+        printf("\n\tEnter Class: ");
+        scanf("%9s", info.Class);
+        printf("\n\tEnter Address: ");
+        scanf("%19s", info.vill);
+        printf("\n\tEnter Percentage: ");
+        scanf("%f", &info.per);
+
+        fwrite(&info, sizeof(struct student), 1, fp);
+        fclose(fp);
+
+        printf("\n\tRecord stored successfully!\n");
+        printf("\n\tDo you want to add another record? (y/n): ");
+        scanf(" %c", &another);
+
+    } while (another == 'y' || another == 'Y');
 }
+
+ 
